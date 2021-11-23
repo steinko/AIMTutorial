e8c863c219a34fcdf110fea1ab066c96049125f1
steinko <steinkor@gmail.com>
2021-11-23 14:58:03 +0100

    add acceptance test

---
+++ features/step_definitions/Login.js
@@ -0,0 +1,32 @@
+const { Given, When, Then } = require('@cucumber/cucumber')
+
+         Given('IAM user exist in AWS', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+
+         When('the program activate login command', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+ 
+         When('user name {string} are enterd', function (string) {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+   
+
+         When('password {string} are entered', function (string) {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+       
+   
+
+         Then('IAM user is login on AWS', function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
